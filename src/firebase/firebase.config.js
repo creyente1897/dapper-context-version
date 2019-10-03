9ff1cd4aa434f9d19c7bc8a6b024c51f2edaa06c
creyente1897 <ayushsrivastava1897@gmail.com>
2019-10-03 17:07:40 +0530

    Initial repository to convert redux over to contextAPI

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBcRIwwkb1Pz-6EKJ7snd9M35EhBHf248w",
+    authDomain: "dapperdb-ee83a.firebaseapp.com",
+    databaseURL: "https://dapperdb-ee83a.firebaseio.com",
+    projectId: "dapperdb-ee83a",
+    storageBucket: "dapperdb-ee83a.appspot.com",
+    messagingSenderId: "294079479462",
+    appId: "1:294079479462:web:b235c7a0259e003fbdbe67"
+};
+
+export default FIREBASE_CONFIG;
